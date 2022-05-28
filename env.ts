9d34ae43072a28c5d61471735d769000627f8607
Hyacinthe Abanda <hyacinthabanda@gmail.com>
2022-05-28 03:01:49 +0100

    add env file

---
+++ env.ts
@@ -0,0 +1,6 @@
+export default {
+    BASE_URL: 'http://infinite-connect.herokuapp.com',
+    TOKEN: 'infinitec_api_token',
+    DATABASE_NAME: 'infinitec_local_db',
+    USER_NAME: 'infinite_user',
+}
