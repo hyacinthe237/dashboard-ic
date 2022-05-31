85e075471c7467b255df92e2be8ec66bcbeaff1d
Hyacinthe Abanda <hyacinthabanda@gmail.com>
2022-06-01 00:43:58 +0100

    remove medical function on kiddataservices

--- src/services/KidDataService.ts
+++ src/services/KidDataService.ts
@@ -4,23 +4,14 @@ class KidDataService {
     getAll(): Promise<any> {
       return http.get("/user-api/kids/");
     }
-    getAllMedicalInfos(): Promise<any> {
-      return http.get("/user-api/medical-infos/");
-    }
     get(id: any): Promise<any> {
       return http.get(`/user-api/kids/${id}/`);
     }
     getKidDetail(id: any): Promise<any> {
       return http.get(`/user-api/kids/${id}/detail`);
     }
-    getMedicalInfos(id: any): Promise<any> {
-      return http.get(`/user-api/kids/${id}/medical-infos`);
-    }
     create(data: any): Promise<any> {
       return http.post("/user-api/kids/", data);
-    }
-    uploadDocMedical(id: any, data: any): Promise<any> {
-      return http.post(`/user-api/kids/${id}/upload-doc-medical-info`, data);
     }
     update(id: any, data: any): Promise<any> {
       return http.patch(`/user-api/kids/${id}/`, data);
