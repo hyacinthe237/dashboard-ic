59b106f0a730090491a85ac91611e6f223eccecd
Hyacinthe Abanda <hyacinthabanda@gmail.com>
2022-06-01 02:02:00 +0100

    modify common http

--- src/http-common.ts
+++ src/http-common.ts
@@ -3,13 +3,15 @@ import env from '../env';
 import AuthService from './middleware/auth';
 import Swal from 'sweetalert2';
 
+const token: any = localStorage.getItem(env.TOKEN)
+
 const apiClient: AxiosInstance = axios.create();
 apiClient.defaults.baseURL = env.BASE_URL
 apiClient.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8'
 apiClient.defaults.headers.common['Accept'] = 'application/json'
 
-if (localStorage.getItem(env.TOKEN) !== null) {
-    apiClient.defaults.headers.common['Authorization'] = 'Token ' + localStorage.getItem(env.TOKEN)
+if (token !== null) {
+    apiClient.defaults.headers.common['Authorization'] = 'Token ' + token
 }
 
 // Intercept all 401
@@ -17,8 +19,8 @@ apiClient.interceptors.response.use(response => {
     return response
 }, error => {
     if (error.response.status === 401) {
-        // AuthService.logout()
-        // location.reload()
+        AuthService.logout()
+        location.reload()
 
         Swal.fire({ title: 'Something wrong', html: error.response.data });
     }
